1468b55e09aa894cf327a52e350897f8db8bce79
ogidazepam <v.grabovskyi10@gmail.com>
2026-02-16 19:25:01 +0100

    Init commit

---
+++ src/main/java/org/example/model/Item.java
@@ -0,0 +1,50 @@
+package org.example.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Item")
+public class Item {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "item_name")
+    private String itemName;
+
+    @ManyToOne
+    @JoinColumn(name = "person_id", referencedColumnName = "id")
+    private Person owner;
+
+    public Item(){}
+
+    public Item(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "id=" + id +
+                ", itemName='" + itemName + '\'' +
+                '}';
+    }
+}
